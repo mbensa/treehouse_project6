d26dda308e1607a63be31ce01e7f330954b64a84
Mojca Bensa <mojca.bensa@gmail.com>
2020-09-17 14:09:46 +0200

    event listener

--- js/app.js
+++ js/app.js
@@ -1,4 +1,17 @@
+//declare variables
 const qwerty = document.getElementById("qwerty");
 const phrase = document.getElementById("phrase");
-const reset = document.querySelectorAll(".btn_reset");
+const btnReset = document.querySelector("a");
+const overlay = document.getElementById("overlay");
 let missed = 0;
+
+//phrases array
+const phrases = ["banana", "pineapple", "passion fruit", "mango", "strawberry"];
+
+//event listener for the "start game" button
+btnReset.addEventListener("click", () => {
+  overlay.style.display = "none";
+});
+
+//function that randombly chooses a phrase from the phrases array
+function getRandomPhraseAsArray(arr) {}
