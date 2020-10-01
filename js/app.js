fd3b2843833c14664d2252e66e2fe6c03fba9d4e
Mojca Bensa <mojca.bensa@gmail.com>
2020-10-01 21:08:04 +0200

    added checkwin function

--- js/app.js
+++ js/app.js
@@ -1,18 +1,15 @@
-//declare variables
+//variables
 const qwerty = document.getElementById("qwerty");
 const phrase = document.getElementById("phrase");
 const btnReset = document.querySelector("a");
 const overlay = document.getElementById("overlay");
 const ol = document.querySelector("ol");
+let title = document.querySelector("h2");
+let phraseArray = getRandomPhraseAsArray(phrases);
 let missed = 0;
 
 //phrases array
 const phrases = ["banana", "pineapple", "passion fruit", "mango", "strawberry"];
-
-//event listener for the "start game" button
-btnReset.addEventListener("click", () => {
-  overlay.style.display = "none";
-});
 
 //function that randombly chooses a phrase from the phrases array and returns a character array
 function getRandomPhraseAsArray(arr) {
@@ -20,8 +17,6 @@ function getRandomPhraseAsArray(arr) {
   const splitPhrase = arr[randomPhrase].split("");
   return splitPhrase;
 }
-
-getRandomPhraseAsArray(phrases);
 
 //check if a character is a letter
 function isCharacterALetter(char) {
@@ -31,24 +26,20 @@ function isCharacterALetter(char) {
 //function that loops through an array of characters and adss them to the list item
 function addPhraseToDisplay(arr) {
   for (let i = 0; i < arr.length; i++) {
+    let li = document.createElement("li");
+    li.setAttribute("id", "phrase");
+
     if (isCharacterALetter(phraseArray[i])) {
-      let li = document.createElement("li");
-      li.setAttribute("id", "phrase");
-      li.setAttribute("class", "letter");
-      li.textContent = phraseArray[i];
-      let ul = document.getElementsByTagName("ul")[0];
-      ul.appendChild(li);
+      li.classList.add("letter");
     } else {
-      let li = document.createElement("li");
-      li.setAttribute("id", "phrase");
-      li.textContent = phraseArray[i];
-      let ul = document.getElementsByTagName("ul")[0];
-      ul.appendChild(li);
+      li.classList.add("space");
     }
+
+    li.textContent = phraseArray[i];
+    let ul = document.getElementsByTagName("ul")[0];
+    ul.appendChild(li);
   }
 }
-let phraseArray = getRandomPhraseAsArray(phrases);
-addPhraseToDisplay(phraseArray);
 
 //checkLetter function
 function checkLetter(btn) {
@@ -56,24 +47,51 @@ function checkLetter(btn) {
   let match = null;
   for (let i = 0; i < allLi.length; i++) {
     if (btn.textContent === allLi[i].textContent) {
-      allLi[i].setAttribute("class", "show");
+      allLi[i].classList.add("show");
       match = btn.textContent;
     }
   }
   return match;
 }
 
+//check whether the game has been won or lost
+function checkWin() {
+  const letter = document.querySelectorAll(".letter");
+  const show = document.querySelectorAll(".show");
+  if (letter.length === show.length) {
+    overlay.classList.add("win");
+    overlay.style.display = "flex";
+    title.textContent = "YOU WON!";
+  } else if (missed > 4) {
+    overlay.classList.add("lose");
+    overlay.style.display = "flex";
+    title.textContent = "YOU LOST!";
+  }
+}
+
+getRandomPhraseAsArray(phrases);
+addPhraseToDisplay(phraseArray);
+
+//event listener for the "start game" button
+btnReset.addEventListener("click", () => {
+  overlay.style.display = "none";
+});
+
 //add an event listener to the keyboard
 qwerty.addEventListener("click", (e) => {
   console.log(e);
   if (e.target.nodeName === "BUTTON") {
     let btn = e.target;
-    btn.setAttribute("class", "chosen");
+    btn.classList.add("chosen");
     btn.disabled = "disabled";
     let letterFound = checkLetter(btn);
-    if (letterFound === null) {
+    if (
+      letterFound === null &&
+      document.querySelectorAll(".tries").length >= 1
+    ) {
       missed += 1;
       ol.removeChild(ol.lastElementChild);
     }
+    checkWin();
   }
 });
